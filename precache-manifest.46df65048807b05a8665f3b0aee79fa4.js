4470799e47834224f72d058e4f0bed37d4699804
BrandonJoe <brandon.joe42@yahoo.com>
2019-05-31 21:50:25 -0700

    Updates

---
+++ precache-manifest.46df65048807b05a8665f3b0aee79fa4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c77c3314be21accdd8432470eb8406c",
+    "url": "/AngelCity/index.html"
+  },
+  {
+    "revision": "ca51f22491ab898aea69",
+    "url": "/AngelCity/static/css/main.6beacb73.chunk.css"
+  },
+  {
+    "revision": "6da711b1950457c79d9b",
+    "url": "/AngelCity/static/js/2.d7fecc60.chunk.js"
+  },
+  {
+    "revision": "ca51f22491ab898aea69",
+    "url": "/AngelCity/static/js/main.772bac91.chunk.js"
+  },
+  {
+    "revision": "5b7a27e5ded081d35b30",
+    "url": "/AngelCity/static/js/runtime~main.89bbcfd7.js"
+  },
+  {
+    "revision": "65e64540003b5bbebc6d54ac14aa3e76",
+    "url": "/AngelCity/static/media/intro2.65e64540.png"
+  },
+  {
+    "revision": "b5d098a074f2cb442f6818885d1ad923",
+    "url": "/AngelCity/static/media/landingback.b5d098a0.jpg"
+  },
+  {
+    "revision": "5e28ad3fc0e2620a8bc261a05c0ad2c5",
+    "url": "/AngelCity/static/media/landingback2.5e28ad3f.jpg"
+  },
+  {
+    "revision": "fc8bc916a1286f30e977457872ac4b6a",
+    "url": "/AngelCity/static/media/phase1.fc8bc916.jpg"
+  },
+  {
+    "revision": "724cc2621cc58c5900aae3abdfc30fcc",
+    "url": "/AngelCity/static/media/phase1back.724cc262.jpg"
+  },
+  {
+    "revision": "e17dfb02e9d2b3465f020b38ac79e3be",
+    "url": "/AngelCity/static/media/phase3.e17dfb02.jpg"
+  }
+]);
